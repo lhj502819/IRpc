c1fa6cfc3d254db1f7c8d96e2f1faeea670e48a3
lhj502819 <54894887+lhj502819@users.noreply.github.com>
2022-12-18 23:06:40 +0800

    Update CuratorZookeeperClient.java

--- irpc-framework-core/src/main/java/cn/onenine/irpc/framework/core/registy/zookeeper/CuratorZookeeperClient.java
+++ irpc-framework-core/src/main/java/cn/onenine/irpc/framework/core/registy/zookeeper/CuratorZookeeperClient.java
@@ -6,9 +6,12 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -78,45 +81,87 @@ public class CuratorZookeeperClient extends AbstractZookeeperClient {
 
     public void createPersistentData(String address, String data) {
         try {
-            client.create().creatingParentContainersIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(address,data.getBytes());
+            client.create().creatingParentContainersIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(address, data.getBytes());
         } catch (Exception e) {
             logger.error("create persistent data error", e);
         }
     }
 
     public void createPersistentWithSeqData(String address, String data) {
-
+        try {
+            client.create().creatingParentContainersIfNeeded().withMode(CreateMode.PERSISTENT_SEQUENTIAL).forPath(address, data.getBytes());
+        } catch (Exception e) {
+            logger.error("create persistent data with seq data error", e);
+        }
     }
 
     public void createTemporarySeqDat(String address, String data) {
-
+        try {
+            client.create().creatingParentContainersIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(address, data.getBytes());
+        } catch (Exception e) {
+            logger.error("create temporary seq data error", e);
+        }
     }
 
     public void createTemporaryData(String address, String data) {
-
+        try {
+            client.create().creatingParentContainersIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(address, data.getBytes());
+        } catch (KeeperException.NoChildrenForEphemeralsException e) {
+            try {
+                client.setData().forPath(address, data.getBytes());
+            } catch (Exception ex) {
+                throw new IllegalStateException(ex.getMessage(), ex);
+            }
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex.getMessage(), ex);
+        }
     }
 
     public void setTemporaryData(String address, String data) {
+        try {
+            client.setData().forPath(address, data.getBytes());
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex.getMessage(), ex);
+        }
 
     }
 
     public void destroy() {
-
+        client.close();
     }
 
     public List<String> listNode(String address) {
-        return null;
+        try {
+            return client.getChildren().forPath(address);
+        } catch (Exception e) {
+            logger.error("list node error", e);
+        }
+        return Collections.emptyList();
     }
 
     public boolean existNode(String address) {
+        try {
+            Stat stat = client.checkExists().forPath(address);
+            return stat != null;
+        } catch (Exception e) {
+            logger.error("check exist node error", e);
+        }
         return false;
     }
 
     public void watchNodeData(String path, Watcher watcher) {
-
+        try {
+            client.getData().usingWatcher(watcher).forPath(path);
+        } catch (Exception e) {
+            logger.error("watch node data error", e);
+        }
     }
 
     public void watchChildNodeData(String path, Watcher watcher) {
-
+        try {
+            client.getData().usingWatcher(watcher).forPath(path);
+        } catch (Exception e) {
+            logger.error("watch child node data error" , e);
+        }
     }
 }
